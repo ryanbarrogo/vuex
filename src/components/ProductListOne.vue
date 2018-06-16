e0c6e84ca7bb9c71e41e252d74e3ce08d401b1b4
Ryan Barrogo <barrogoryan@gmail.com>
2018-06-16 17:18:06 +0800

    Added axios

--- src/components/ProductListOne.vue
+++ src/components/ProductListOne.vue
@@ -31,36 +31,66 @@
         </v-flex>
       </v-layout>
     </v-container>
+    <!--<ul v-if="posts && posts.length">
+      <li v-for="post of posts">
+        <p><strong>{{post.title}}</strong></p>
+        <p>{{post.body}}</p>
+      </li>
+    </ul>-->
+
+    <ul v-if="errors && errors.length">
+      <li v-for="error of errors">
+        {{error.message}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
   import {mapActions} from 'vuex'
   import {mapGetters} from 'vuex'
+  import axios from 'axios'
 
   export default {
     name: "ProductListOne",
     data() {
-      return {}
+      return {
+        posts: [],
+        errors: []
+      }
+    },
+    // Fetches posts when the component is created.
+    created() {
+      /*axios.get(`http://jsonplaceholder.typicode.com/posts`)
+        .then(response => {
+          // JSON responses are automatically parsed.
+          this.posts = response.data
+        })
+        .catch(e => {
+          this.errors.push(e)
+        })*/
+
+      /*async / await version (created() becomes async created())
+
+      try {
+        const response = await axios.get(`http://jsonplaceholder.typicode.com/posts`)
+        this.posts = response.data
+      } catch (e) {
+        this.errors.push(e)
+      }*/
     },
     computed:{
       products(){
         return this.$store.state.products
       },
-      saleProducts(){
-        return this.$store.getters.saleProducts
-      },
-      /*...mapGetters({
-
-      })*/
+      ...mapGetters([
+        'saleProducts'
+      ])
     },
     methods:{
-      reducePrice: function (amount) {
-        this.$store.dispatch('reducePrice', amount)
-      },
-      /*...mapActions({
-        'saleProducts'
-      })*/
+      ...mapActions([
+        'reducePrice'
+      ])
     }
   }
 </script>
